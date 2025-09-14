a10bb206e9478deb47a43c8fec66892d6f5a6e65
Juan Esteban Torres <jetorresp@eafit.edu.co>
2025-09-14 01:37:42 -0500

    Update Lambda.hs

--- Lambda.hs
+++ Lambda.hs
@@ -1,45 +1,31 @@
-module Main where
+module ChurchBool where
 
--- Definimos el tipo de Booleanos de Church
-type ChurchBool a = a -> a -> a
-
--- true y false como funciones
-true :: ChurchBool a
+true :: a -> a -> a
 true = \x y -> x
 
-false :: ChurchBool a
+false :: a -> a -> a
 false = \x y -> y
 
--- ifThenElse
-ifThenElse :: ChurchBool a -> a -> a -> a
-ifThenElse b m n = b m n
+ifThenElse :: (a -> a -> a) -> a -> a -> a
+ifThenElse = \b m n -> b m n
 
--- AND
-churchAnd :: ChurchBool (ChurchBool a) -> ChurchBool a -> ChurchBool a
-churchAnd p q = p q false
+churchAnd = \a b -> a b false
 
--- OR
-churchOr :: ChurchBool (ChurchBool a) -> ChurchBool a -> ChurchBool a
-churchOr p q = p true q
+churchOr = \a b -> a true b
 
--- NOT
-churchNot :: ChurchBool (ChurchBool a) -> ChurchBool a
-churchNot p = p false true
+churchNot = \b -> b false true
 
--- Mostrar ChurchBool como String
-showBool :: ChurchBool String -> String
+showBool :: (String -> String -> String) -> String
 showBool b = b "True" "False"
 
--- Pruebas
 test :: IO ()
 test = do
-  putStrLn $ "true AND false = "               ++ showBool (churchAnd true false)
-  putStrLn $ "true OR  false = "               ++ showBool (churchOr  true false)
-  putStrLn $ "NOT true        = "              ++ showBool (churchNot true)
-  putStrLn $ "NOT false       = "              ++ showBool (churchNot false)
-  putStrLn $ "ifThenElse true  \"yes\" \"no\" = "  ++ ifThenElse true  "yes" "no"
-  putStrLn $ "ifThenElse false \"yes\" \"no\" = "  ++ ifThenElse false "yes" "no"
+    putStrLn $ "true AND false           = " ++ showBool (churchAnd true false)
+    putStrLn $ "true OR false            = " ++ showBool (churchOr true false)
+    putStrLn $ "NOT true                 = " ++ showBool (churchNot true)
+    putStrLn $ "NOT false                = " ++ showBool (churchNot false)
+    putStrLn $ "ifThenElse true \"yes\" \"no\" = " ++ ifThenElse true "yes" "no"
+    putStrLn $ "ifThenElse false \"yes\" \"no\"= " ++ ifThenElse false "yes" "no"
 
--- Punto de entrada
 main :: IO ()
 main = test
